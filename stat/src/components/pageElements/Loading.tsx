3e5f57251fb2c98e20996c9d8ee2865f54ce5a13
Daniel Gergely <daniel.gergely@uzh.ch>
2024-10-18 12:31:08 +0200

    changed loading animation #33

--- stat/src/components/pageElements/Loading.tsx
+++ stat/src/components/pageElements/Loading.tsx
@@ -1,22 +1,26 @@
 import styled from "styled-components";
+import { ReactComponent as LoadingSVG} from "../../assets/loading.svg";
 
 const StyledLoadingContainer = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background: #ffffff80;
-  z-index: 100;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    background: #ffffff20;
+    z-index: 100;
+    
+    & svg {
+        width: 500px;
+        height: 500px;
+    }
 `;
 
 export function Loading() {
     return (
         <StyledLoadingContainer>
-            <div className="spinner-border text-primary" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </div>
+            <LoadingSVG />
         </StyledLoadingContainer>
     )
 }
