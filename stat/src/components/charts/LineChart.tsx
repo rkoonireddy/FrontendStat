45f80f8261ed8535968d180c12eef52423884d29
Daniel Gergely <daniel.gergely@uzh.ch>
2024-11-03 23:15:08 +0100

    minor changes/corrections

--- stat/src/components/charts/LineChart.tsx
+++ stat/src/components/charts/LineChart.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import {
     axisBottom,
     axisLeft,
@@ -9,19 +9,18 @@ import {
     min,
     max,
     zoom,
-    ZoomBehavior
+    ZoomBehavior, zoomIdentity
 } from "d3";
-import { useAppSelector } from "../../hooks";
-import { getPipeline } from "../../redux/pipelineSlice";
-import { BlockModel } from "../../types/responseType";
-import { DataDocument} from "../../types/dataType";
+import {useAppSelector} from "../../hooks";
+import {getPipeline} from "../../redux/pipelineSlice";
+import {BlockModel} from "../../types/responseType";
+import {DataDocument} from "../../types/dataType";
+import {convertToDataDocument} from "../../util/util";
 
 
+// Viridis colors - color blind color palette
 const DEFAULT_COLORS = [
-    "#00bfa6", "#e41a1c", "#4daf4a", "#984ea3", "#ff7f00",
-    "#ffff33", "#a65628", "#f781bf", "#999999", "#E69F00",
-    "#0072B2", "#CC79A7", "#999999", "#C5B0D5", "#C45E24",
-    "#009E73", "#F0E442", "#56B4E9", "#D55E00"
+    "#fde725", "#90d743", "#35b779", "#21918c", "#31688e", "#443983", "#440154"
 ];
 
 interface LineChartProps {
@@ -30,7 +29,7 @@ interface LineChartProps {
     mini?: boolean;
 }
 
-export function LineChart({ block, small = false, mini = false }: LineChartProps) {
+export function LineChart({block, small = false, mini = false}: LineChartProps) {
     const pipeline = useAppSelector(getPipeline);
     const [chartData, setChartData] = useState<DataDocument[]>([]);
     const [legendLabels, setLegendLabels] = useState<string[]>([]);
@@ -38,14 +37,7 @@ export function LineChart({ block, small = false, mini = false }: LineChartProps
 
     useEffect(() => {
         if (block?.output?.Dataframe?.data) {
-            //creates cleaner d3 input data
-            const dataArray = block.output.Dataframe.data[0].data.data.map((_, rowIndex) => {
-                const rowObject: DataDocument = {};
-                block.output.Dataframe.data.forEach(column => {
-                    rowObject[column.name] = column.data.data[rowIndex] as number | null;
-                });
-                return rowObject;
-            });
+            const dataArray = convertToDataDocument(block.output.Dataframe.data);
             setChartData(dataArray);
 
             const columnNames = Object.keys(dataArray[0] || {}).slice(1);
@@ -56,8 +48,7 @@ export function LineChart({ block, small = false, mini = false }: LineChartProps
         }
     }, [block, pipeline]);
 
-    // Refs
-    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+    const [dimensions, setDimensions] = useState({width: 0, height: 0});
     const svgRef = useRef<SVGSVGElement | null>(null);
 
     useEffect(() => {
@@ -66,8 +57,8 @@ export function LineChart({ block, small = false, mini = false }: LineChartProps
 
         const resizeObserver = new ResizeObserver(entries => {
             if (!entries || entries.length === 0) return;
-            const { width, height } = entries[0].contentRect;
-            setDimensions({ width, height });
+            const {width, height} = entries[0].contentRect;
+            setDimensions({width, height});
         });
 
         resizeObserver.observe(svgElement);
@@ -83,10 +74,10 @@ export function LineChart({ block, small = false, mini = false }: LineChartProps
         if (!svgRef.current || dimensions.width === 0 || dimensions.height === 0) return;
         const svg = select(svgRef.current);
 
-        let margin = { top: 20, right: 10, bottom: 50, left: 50 };
+        let margin = {top: 20, right: 10, bottom: 50, left: 50};
         if (small) {
-            if(mini) {
-                margin = { top: 2, right: 2, bottom: 2, left: 2 };
+            if (mini) {
+                margin = {top: 2, right: 2, bottom: 2, left: 2};
             } else {
                 margin = {top: 10, right: 10, bottom: 25, left: 25};
             }
@@ -146,7 +137,20 @@ export function LineChart({ block, small = false, mini = false }: LineChartProps
                 });
             });
 
+        const resetGraph = () => {
+            if (svgRef.current) {
+                const svg = select(svgRef.current);
+                svg.transition().duration(250).call(
+                    zoomBehavior.transform,
+                    zoomIdentity,
+                    select(svgRef.current).select("g").node()
+                );
+            }
+        };
+
         svg.call(zoomBehavior);
+        svg.on("dblclick.zoom", null);
+        svg.on("dblclick", resetGraph);
 
         //axes
         svg.select<SVGGElement>(".x-axis")
@@ -182,13 +186,13 @@ export function LineChart({ block, small = false, mini = false }: LineChartProps
                 .style("cursor", "pointer")
                 .attr("transform", `translate(${margin.left}, ${margin.top})`)
                 .attr("stroke", DEFAULT_COLORS[lineIndex % DEFAULT_COLORS.length])
-                .attr("stroke-opacity", selectedLineIndex === null || selectedLineIndex === lineIndex ? 2 : 0.25)
+                .attr("stroke-opacity", selectedLineIndex === null || selectedLineIndex === lineIndex ? 1 : 0.25)
                 .attr("stroke-width", selectedLineIndex === lineIndex ? "3px" : "1px")
                 .attr("stroke-width", small || mini ? "1px" : "2px")
                 .on("click", () => setSelectedLineIndex(selectedLineIndex === lineIndex ? null : lineIndex));
         });
 
-        if(small || mini) {
+        if (small || mini) {
             svg.select<SVGGElement>(".x-axis")
                 .selectAll("text")
                 .style("font-size", "7px")
@@ -199,117 +203,103 @@ export function LineChart({ block, small = false, mini = false }: LineChartProps
                 .style("font-size", "7px")
                 .style("font-weight", "lighter");
         }
-        
+
         const legendGroup = svg.select<SVGGElement>(".legend-group").empty()
-        ? svg.append("g").attr("class", "legend-group").attr("transform", `translate(${dimensions.width - margin.right - 20}, ${margin.top})`)
-        : svg.select<SVGGElement>(".legend-group");
-    
-    if (!small && !mini){
-        svg.selectAll(".x-axis-label")
-        .data([null])
-        .join("text")
-        .attr("class", "x-axis-label")
-        .attr("text-anchor", "middle")
-        .attr("x", width / 2 + margin.left)
-        .attr("y", height + margin.top + 40)
-        .style("fill", "white")
-        .text("Time (as index)");
+            ? svg.append("g").attr("class", "legend-group").attr("transform", `translate(${dimensions.width - margin.right - 20}, ${margin.top})`)
+            : svg.select<SVGGElement>(".legend-group");
 
-        svg.selectAll(".y-axis-label")
-        .data([null])
-        .join("text")
-        .attr("class", "y-axis-label")
-        .attr("text-anchor", "middle")
-        .attr("x", -(height / 2 + margin.top))
-        .attr("y", margin.left - 35)
-        .attr("transform", "rotate(-90)")
-        .style("fill", "white")
-        .text("Amplitude");
+        if (!small && !mini) {
+            svg.selectAll(".x-axis-label")
+                .data([null])
+                .join("text")
+                .attr("class", "x-axis-label")
+                .attr("text-anchor", "middle")
+                .attr("x", width / 2 + margin.left)
+                .attr("y", height + margin.top + 40)
+                .style("fill", "white")
+                .text("Time (as index)");
 
-        legendGroup.selectAll(".legend-item").remove();
-        
-        legendLabels.forEach((label, lineIndex) => {
-            legendGroup.append("foreignObject")
-                .attr("x", -140)
-                .attr("y", (lineIndex-0.5) * 20+5)
-                .attr("width", 20)
-                .attr("height", 20)
-                .append("xhtml:input")
-                .attr("type", "checkbox")
-                .attr("id", `checkbox-${lineIndex}`)
-                .property("checked", selectedLineIndex === lineIndex)
-                .style("cursor", "pointer")
-                .on("change", function () {
-                    const index = lineIndex;
+            svg.selectAll(".y-axis-label")
+                .data([null])
+                .join("text")
+                .attr("class", "y-axis-label")
+                .attr("text-anchor", "middle")
+                .attr("x", -(height / 2 + margin.top))
+                .attr("y", margin.left - 35)
+                .attr("transform", "rotate(-90)")
+                .style("fill", "white")
+                .text("Amplitude");
+
+            legendGroup.selectAll(".legend-item").remove();
+
+            legendLabels.forEach((label, lineIndex) => {
+                legendGroup.append("text")
+                    .attr("x", -120)
+                    .attr("y", lineIndex * 20 + 5)
+                    .attr("dy", "0.35em")
+                    .style("fill", DEFAULT_COLORS[lineIndex % DEFAULT_COLORS.length])
+                    .style("cursor", "pointer")
+                    .style("text-anchor", "start")
+                    .text(label)
+                    .on("click", function (event: MouseEvent) {
+                        const index = lineIndex;
+                        if (selectedLineIndex === index) {
+                            setSelectedLineIndex(null);
+                        } else {
+                            setSelectedLineIndex(index);
+                        }
+                    });
+            });
+
+            legendGroup.selectAll("text")
+                .data(legendLabels)
+                .join("text")
+                .on("click", function (event: MouseEvent, label: string) {
+                    const index = legendLabels.indexOf(label);
                     if (selectedLineIndex === index) {
                         setSelectedLineIndex(null);
                     } else {
                         setSelectedLineIndex(index);
                     }
-                });
-
-            legendGroup.append("text")
+                })
                 .attr("x", -120)
-                .attr("y", lineIndex * 20+5)
+                .attr("y", (d, i) => i * 20 + 5)
                 .attr("dy", "0.35em")
-                .style("fill", DEFAULT_COLORS[lineIndex % DEFAULT_COLORS.length])
+                .attr("opacity", (d, i) => selectedLineIndex === i ? 1 : 0.25)
+                .style("fill", (d, i) => DEFAULT_COLORS[i % DEFAULT_COLORS.length])
                 .style("cursor", "pointer")
                 .style("text-anchor", "start")
-                .text(label)
-                .on("click", function (event: MouseEvent) {
-                    const index = lineIndex;
-                    if (selectedLineIndex === index) {
-                        setSelectedLineIndex(null);
-                    } else {
-                        setSelectedLineIndex(index);
-                    }
-                });
-        });
-    }
+                .text(d => d);
+        }
 
     }, [chartData, dimensions, legendLabels, selectedLineIndex, mini, small]);
 
-    const resetGraph = () => {
-        if (svgRef.current) {
-            const svg = select(svgRef.current);
-            svg.selectAll("*").remove();
-            window.location.reload();
-        }
-    };
-    
+
     return (
-        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-    {block?.output?.Dataframe?.data !== undefined ? (
-        <div style={{ width: '100%', height: '100%' }}>
-                <svg 
-                    ref={svgRef} 
-                    width="100%" 
-                    height="100%" 
-                    style={{ display: 'block' }} 
-                >
-                    <g className="x-axis" />
-                    <g className="y-axis" />
-                    <g className="chart-group" />
-                </svg>
-                {!(small || mini) && (
-                    <button 
-                        onClick={resetGraph} 
-                        style={{ 
-                            position: 'absolute', 
-                            bottom: '10px',
-                            right: '10px',
-                            cursor: 'pointer' 
-                        }}
+        <div style={{width: '100%', height: '100%', position: 'relative'}}>
+            {block?.output?.Dataframe?.data !== undefined ? (
+                <div style={{width: '100%', height: '100%'}}>
+                    <svg
+                        ref={svgRef}
+                        width="100%"
+                        height="100%"
+                        style={{display: 'block'}}
                     >
-                        Reset Graph
-                    </button>
-                )}
-            </div>
-        ) : (
-            <div style={{ color: "#ffffff", alignContent: "center", textAlign: "center", transform: small ? 'scale(0.5)' : 'scale(1)' }}>
-                {small ? 'Run Pipeline' : 'Please run the pipeline to visualize your data!'}
-            </div>
-        )}
-    </div>
+                        <g className="x-axis"/>
+                        <g className="y-axis"/>
+                        <g className="chart-group"/>
+                    </svg>
+                </div>
+            ) : (
+                <div style={{
+                    color: "#ffffff",
+                    alignContent: "center",
+                    textAlign: "center",
+                    transform: small ? 'scale(0.5)' : 'scale(1)'
+                }}>
+                    {small ? 'Run Pipeline' : 'Please run the pipeline to visualize your data!'}
+                </div>
+            )}
+        </div>
     );
 }
