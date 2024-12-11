2fc34f3cb09fbe05e41191a5119c9e35a7156a56
rkoonireddy <rohit.koonireddy@gmail.com>
2024-12-11 23:31:22 +0100

    featrues and filters functionality added to comparing blocks

--- stat/src/components/sections/CompareCharts.tsx
+++ stat/src/components/sections/CompareCharts.tsx
@@ -57,6 +57,25 @@ const StyledRunBlockMsg = styled.div`
     font-style: italic;
 `;
 
+const StyledBlockContent = styled.div`
+    font-size: 0.75rem;
+    font-weight: thin;
+    color: #73B5B4;
+    margin-left: 5px;
+`;
+
+const StyledBlockSubHead = styled.div`
+    font-size: 0.75rem;
+    font-weight: thin;
+    color: white;
+    padding-bottom: 5px;
+    margin-left: 5px;
+    text-decoration: underline;
+    text-decoration-color: #ccc;
+    text-decoration-thickness: 2px;
+`;
+
+
 export function CompareCharts() {
     const blocks = useAppSelector((state) => state.pipeline.blocks) || []; // Fallback to empty array if undefined
     const [selectedFilters, setSelectedFilters] = useState<{ [key: string]: string[] }>({});
@@ -84,31 +103,67 @@ export function CompareCharts() {
             <StyledBlockIdLabel>Choose features on different blocks to compare</StyledBlockIdLabel>
             {filteredBlocks.length > 0 ? (
                 <HorizontalScrollContainer id={"compare-charts"}>
-                    {filteredBlocks.map((block: BlockModel, index) => (
-                        <StyledBlockLineSelectorContainer key={block.id}
-                                                          $borderColor={COLOR_PALETTE[index % COLOR_PALETTE.length]}>
-                            <StyledBlockIdLabel>{block.name}</StyledBlockIdLabel>
-                            <StyledCheckboxContainer>
-                                {block.cols_to_process ?
-                                (Object.values(block.cols_to_process) as string[]).map((filterName) => (
-                                    <div key={filterName} style={{display: 'flex', alignItems: 'center'}}>
-                                        <input
-                                            type="checkbox"
-                                            checked={selectedFilters[block.id]?.includes(filterName) || false}
-                                            onChange={() => handleCheckboxChange(block.id, filterName)}
-                                        />
-                                        <StyledLabel>{filterName}</StyledLabel>
-                                    </div>
-                                )) : <StyledRunBlockMsg> Run block to see available columns </StyledRunBlockMsg>}
-                            </StyledCheckboxContainer>
-                        </StyledBlockLineSelectorContainer>
-                    ))}
+                    {filteredBlocks.map((block: BlockModel, index) => {
+                        const filterKeys = Object.keys(block.filters).filter(key => key !== "cols_to_process");
+                        return (
+                            <StyledBlockLineSelectorContainer
+                                key={block.id}
+                                $borderColor={COLOR_PALETTE[index % COLOR_PALETTE.length]}
+                            >
+                                <StyledBlockIdLabel>{block.name}</StyledBlockIdLabel>
+                                <StyledBlockContent>{block.type}</StyledBlockContent>
+                                <StyledCheckboxContainer>
+                                    {block.cols_to_process ? (
+                                        <>
+                                            <div>
+                                                <StyledBlockSubHead>Features</StyledBlockSubHead>
+                                                {(Object.values(block.cols_to_process) as string[]).map(featureName => (
+                                                    <div key={featureName} style={{ display: 'flex', alignItems: 'center' }}>
+                                                        <input
+                                                            type="checkbox"
+                                                            checked={selectedFilters[block.id]?.includes(featureName) || false}
+                                                            onChange={() => handleCheckboxChange(block.id, featureName)}
+                                                        />
+                                                        <StyledLabel style={{ color: 'white' }}>{featureName}</StyledLabel>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                            <hr style={{ margin: '0px 0', border: '1px solid #ccc' }} />
+    
+                                            <div style={{alignItems: 'center' }}>
+                                                <StyledBlockSubHead>Filters</StyledBlockSubHead>
+                                                {filterKeys.length > 0 ? (
+                                                    <ul style={{ margin: 0, padding: '0 0 0 20px', textAlign: 'left', color: '#73B5B4'}}> {/* Increase padding to ensure bullets are indented */}
+                                                        {filterKeys.map(filterKey => {
+                                                            const filterValue = block[filterKey as keyof BlockModel];
+                                                            return (
+                                                                <li key={filterKey} style={{ listStyleType: 'disc', marginBottom: '5px' }}>  {/* Bullet points and spacing */}
+                                                                    <StyledBlockContent>
+                                                                        {filterKey}: {JSON.stringify(filterValue)}
+                                                                    </StyledBlockContent>
+                                                                </li>
+                                                            );
+                                                        })}
+                                                    </ul>
+                                                ): (
+                                                    <StyledBlockContent>--- No filters applied ---</StyledBlockContent>
+                                                )}
+                                            </div>
+                                        </>
+                                    ) : (
+                                        <StyledRunBlockMsg>Run block to see available columns</StyledRunBlockMsg>
+                                    )}
+                                </StyledCheckboxContainer>
+                            </StyledBlockLineSelectorContainer>
+                        );
+                    })}
                 </HorizontalScrollContainer>
             ) : (
                 <div>No available data blocks to compare at this time.</div>
             )}
-
-            {Object.keys(selectedFilters).length > 0 && <CompareLineChart selectedFilters={selectedFilters}/>}
+    
+            {Object.keys(selectedFilters).length > 0 && <CompareLineChart selectedFilters={selectedFilters} />}
         </StyledBlocksContainer>
     );
+  
 }
